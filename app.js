3ef86c2c49b0e7a9d356af02ee023ad14ae7c7b2
l33th <l33th@protonmail.ch>
2021-12-18 08:38:53 +0200

    added port env

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World');
+    res.send('Hello, World !');
 });
 
 app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 })
 
-app.listen(3000, () => console.log('Listening on port 3000...'))
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
