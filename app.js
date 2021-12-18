035670390898ce5cec72ad1d284d1f9606670692
l33th <l33th@protonmail.ch>
2021-12-18 08:41:16 +0200

    added id to courses route

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 })
 
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id)
+})
+
 // PORT
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
