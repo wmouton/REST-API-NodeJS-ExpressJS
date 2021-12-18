af71bd7731ddc9a613d2e927bd71604340f72706
l33th <l33th@protonmail.ch>
2021-12-18 08:45:43 +0200

    added month and year courses route

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World !');
+  res.send('Hello, World !');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
-})
+  res.send([1, 2, 3]);
+});
 
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id)
-})
+  res.send(req.params.id);
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+  res.send(req.params);
+});
 
 // PORT
 const port = process.env.PORT || 3000;
