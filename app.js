42e587805daef0fe870ff0b9b14f7c8d970eb5bd
l33th <l33th@protonmail.ch>
2021-12-18 09:00:24 +0200

    if statement and status check

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+  {
+    id: 1,
+    name: 'course1',
+  },
+  {
+    id: 2,
+    name: 'course2',
+  },
+  {
+    id: 3,
+    name: 'course3',
+  },
+];
+
 app.get('/', (req, res) => {
   res.send('Hello, World !');
 });
 
 app.get('/api/courses', (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
-  res.send(req.params.id);
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was not found.');
+  res.send(course);
 });
 
 app.get('/api/posts/:year/:month', (req, res) => {
