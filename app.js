55ee7fdd9b73a03e4f3a165ca065cfaa8ee3f0d2
l33th <l33th@protonmail.ch>
2021-12-18 08:31:14 +0200

    added /api/courses route to fetch array data

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ app.get('/', (req, res) => {
     res.send('Hello, World');
 });
 
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
+})
+
 app.listen(3000, () => console.log('Listening on port 3000...'))
