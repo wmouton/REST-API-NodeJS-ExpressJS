dd8e3ffaa0325c773f382714e5ae93b92bc54cc7
l33th <l33th@protonmail.ch>
2021-12-21 08:17:14 +0200

    post request

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ app.post('/api/courses', (req, res) => {
     id: courses.length + 1,
     name: req.body.name,
   };
+  courses.push(course);
+  res.send(course);
 });
 
 app.get('/api/courses/:id', (req, res) => {
