4b46c5506d886804fd80574c61a25a54b797c063
l33th <l33th@protonmail.ch>
2021-12-22 12:27:15 +0200

    input validation basic

--- app.js
+++ app.js
@@ -1,47 +1,61 @@
+// imports
 const express = require('express');
 const app = express();
 
+// json middleware
 app.use(express.json());
 
-const courses = [
+// cryptocurrencies object
+const cryptocurrencies = [
   {
     id: 1,
-    name: 'course1',
+    name: 'Bitcoin',
   },
   {
     id: 2,
-    name: 'course2',
+    name: 'Ethereum',
   },
   {
     id: 3,
-    name: 'course3',
+    name: 'L33thcoin',
   },
 ];
 
+// GET request for the home/root page
 app.get('/', (req, res) => {
   res.send('Hello, World !');
 });
 
-app.get('/api/courses', (req, res) => {
-  res.send(courses);
+// GET request for all cryptocurrencies
+app.get('/api/cryptocurrencies', (req, res) => {
+  res.send(cryptocurrencies);
 });
 
-app.post('/api/courses', (req, res) => {
-  const course = {
-    id: courses.length + 1,
+// POST request for all the cryptocurrencies testing endpoints to get cryptocurrencies
+app.post('/api/cryptocurrencies', (req, res) => {
+  // Input Validation
+  if (!req.body.name || req.body.length < 3) {
+    // 400 Bad Request
+    res.status(400).send('Name is required and should have a minimum of 3 characters.');
+    return;
+  }
+  const crypto = {
+    id: cryptocurrencies.length + 1,
     name: req.body.name,
   };
-  courses.push(course);
-  res.send(course);
+  cryptocurrencies.push(crypto);
+  res.send(crypto);
 });
 
-app.get('/api/courses/:id', (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course)
-    res.status(404).send('The course with the given ID was not found.');
-  res.send(course);
+// GET request to get cryptocurrencies by id
+app.get('/api/cryptocurrencies/:id', (req, res) => {
+  const crypto = cryptocurrencies.find(c => c.id === parseInt(req.params.id));
+  if (!crypto)
+    res.status(404).send('The crypto with the given ID was not found.');
+  res.send(crypto);
 });
 
+// GET request for the year and month
 app.get('/api/posts/:year/:month', (req, res) => {
   res.send(req.query);
 });
